0ad9921a517819b382ce878266bd12acb581922e
anghelos <angheloc@gmail.com>
2024-05-02 13:32:13 -0400

    Changed presets

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -4018,12 +4018,12 @@
   #define CUSTOM_MENU_MAIN_SCRIPT_RETURN   // Return to status screen after a script
   // #define CUSTOM_MENU_MAIN_ONLY_IDLE         // Only show custom menu when the machine is idle
 
-  #define MAIN_MENU_ITEM_1_DESC "Dry PLA - 2 hours"
-  #define MAIN_MENU_ITEM_1_GCODE "M190 S60\nM75\nM117 Drying for 2 hours\nG4 S7200\nM140 S0\nM73 P100\nM77"
+  #define MAIN_MENU_ITEM_1_DESC "Dry PLA - 3 hours"
+  #define MAIN_MENU_ITEM_1_GCODE "M190 S55\nM75\nM117 Drying PLA\nG4 S10800\nM140 S0\nM73 P100\nM77"
   //#define MAIN_MENU_ITEM_1_CONFIRM          // Show a confirmation dialog before this action
 
-  #define MAIN_MENU_ITEM_2_DESC "Dry PLA - 5 hours"
-  #define MAIN_MENU_ITEM_2_GCODE "M190 S60\nM75\nM117 Drying for 5 hours\nG4 S7200\nM73 P40\nG4 S7200\nM73 P 80\nG4 S3600\nM140 S0\nM73 P100\nM77"
+  #define MAIN_MENU_ITEM_2_DESC "Dry PETG - 3 hours"
+  #define MAIN_MENU_ITEM_2_GCODE "M190 S65\nM75\nM117 Drying PETG\nG4 S10800\nM140 S0\nM73 P100\nM77"
   //#define MAIN_MENU_ITEM_2_CONFIRM
 
   #define MAIN_MENU_ITEM_3_DESC "Please reset to change setting"
